37cbe231d7d2f2525fdd5592ae92b1484c658120
Isaac Mattern <isaac.mattern@gmail.com>
2022-06-13 11:33:09 -0400

    begin work on proj 5

--- 5-meme-generator/src/components/Header.js
+++ 5-meme-generator/src/components/Header.js
@@ -2,7 +2,9 @@ export default function Header() {
 
   return (
     <div className="header">
-      <h1>Meme Generator</h1>
+      <div className="container">
+        <h1>Meme Generator</h1>
+      </div>
     </div>
   )
 }
--- 5-meme-generator/src/components/Meme.js
+++ 5-meme-generator/src/components/Meme.js
@@ -1,8 +1,45 @@
+import React from 'react'
+import memesData from "../memesData"
+
 export default function Meme() {
+
+  let [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg'
+  })
+
+  let [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes
+    let index = Math.floor(Math.random() * memesArray.length)
+    let url = memesArray[index].url
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        randomImage: url
+      }
+    })
+  }
 
   return (
     <div className="meme">
+      <div className="container">
 
+        <div className="form">
+          <div className="form-inputs-container">
+            <input className="form-input" type="text" placeholder="top text"></input>
+            <input className="form-input" type="text" placeholder="bottom text"></input>
+          </div>
+          <button onClick={getMemeImage}>Get a new meme image</button>
+        </div>
+
+        <div className='meme-image'>
+          <img src={meme.randomImage} alt="meme"></img>
+        </div>
+
+      </div>
     </div>
   )
 }
